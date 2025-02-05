8e32ae938554733178b178d1b076dd2d3e3df351
LaurensSluyterman <Laurens.sluyterman@hotmail.com>
2025-02-05 10:55:03 +0100

    changed metric to CRPS

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
-from shiny.types import FileInfo
 
 from plots_and_metrics import (
     calculate_metrics_comparison,
@@ -11,7 +10,7 @@ from plots_and_metrics import (
 
 ### UI ###
 app_ui = ui.page_fluid(
-    ui.h2("Wasserstein distance calculator"),
+    ui.h2("CRPS score calculator"),
     ui.input_select(
         "user_choice",
         "I want to:",
@@ -36,9 +35,7 @@ def server(input: Inputs, output: Outputs, session: Session):
                             Upload the observed values and predicted values in separate csv files (no headers) below:
                             """
                 ),
-                ui.input_file(
-                    "observations", "Observations", accept=[".csv"], multiple=False
-                ),
+                ui.input_file("observations", "Observations", accept=[".csv"], multiple=False),
                 ui.input_file(
                     "predictions_model_1",
                     "Predictions",
@@ -46,19 +43,17 @@ def server(input: Inputs, output: Outputs, session: Session):
                     multiple=False,
                 ),
                 ui.output_text_verbatim("calc_result_single"),
-                ui.output_plot("data_plot_single", width="1000px", height="1000px"),
+                ui.output_plot("data_plot_single", width="700px", height="700px"),
             )
         if input.user_choice() == "Compare two models":
             return ui.div(
                 ui.h3("Comparing two models"),
                 ui.markdown(
-                    """
+                    """ 
                             Upload the observed values and predicted values of both models in separate csv files (no headers) below:
                             """
                 ),
-                ui.input_file(
-                    "observations", "Observations", accept=[".csv"], multiple=False
-                ),
+                ui.input_file("observations", "Observations", accept=[".csv"], multiple=False),
                 ui.input_file(
                     "predictions_model_1",
                     "Predictions Model 1",
@@ -76,10 +71,12 @@ def server(input: Inputs, output: Outputs, session: Session):
                             Three different plots are displayed. The top one gives a histogram of the data points along with 
                             estimated probability density functions of both the observations and both models. These are estimated using
                             a Gaussian Kernel Density estimator using the Silvestor bandwidth rule. The two figures below provide
-                            the emperical CDFs of both models compared to the emperical CDF of the observations. The area between the two, the 
-                            Wasserstein distance, is reported for both models with an accompanying 90% CI (calculated using a bootstrap of size 10,000). The ratio
-                            of the two models is also reported, also with an accompanying 90% CI made from the same bootstrap samples. A value of 1 indicates equal performance, a value below
-                            1 indicates model 1 is superior, a value above 1 indicates model 2 is superior. 
+                            the empirical CDFs of both models compared to the emperical CDF of the observations. The average CRPS score
+                            is reported for both models with an accompanying 90% CI (calculated using a bootstrap of size 10,000). The ratio
+                            of the two models is also reported, also with an accompanying 90% CI made from the same bootstrap samples. 
+                            A value of 1 indicates equal performance, a value below 1 indicates model 1 is superior, a value above 1 indicates model 2 is superior. 
+                            For each model, the skill score gives 1 - CRPS_model / CRPS_naive, 
+                            where CRPS_naive is the CRPS of a model that always predicts the sample median.
 
                             Lastly, the predicted-to-observed geometric mean ratio (that is, the geometric mean of the predictions divided by the geometric mean of
                             the observations) is provided for both models with an accompanying 90% CI. This CI is calculated parametrically (see https://doi.org/10.1007/s40262-023-01326-3 
@@ -87,7 +84,7 @@ def server(input: Inputs, output: Outputs, session: Session):
                             """
                 ),
                 ui.output_text_verbatim("calc_result_comparison"),
-                ui.output_plot("data_plot_comparison", width="1800px", height="2500px"),
+                ui.output_plot("data_plot_comparison", width="1300px", height="1800px"),
             )
 
     @reactive.Calc
@@ -132,13 +129,13 @@ def server(input: Inputs, output: Outputs, session: Session):
         if observations is None or model_1_predictions is None:
             return "Please upload both files to see the result."
 
-        # Perform a simple calculation (e.g., sum of values in both files times multiplier)
-        GMR, CI_GMR, W1, CI_W1 = calculate_metrics_single(
+        GMR, CI_GMR, CRPS_results = calculate_metrics_single(
             model_1_predictions=model_1_predictions, observations=observations
         )
         return f"""
-        Geometric-mean ratio: {GMR} {CI_GMR} \n
-        Wasserstein distance: {W1} {CI_W1}"""
+        Geometric-mean ratio:            {GMR} {[float(CI_GMR[0]), float(CI_GMR[1])]} \n
+        Average CRPS:                    {CRPS_results['CRPS_1']} {[float(CRPS_results['CI(CRPS_1)'][0]), float(CRPS_results['CI(CRPS_1)'][1])]} \n
+        CRPS Skill score:                {CRPS_results['S(CRPS_1)']} {[float(CRPS_results['CI(S(CRPS_1))'][0]), float(CRPS_results['CI(S(CRPS_1))'][1])]} """
 
     @output
     @render.plot
@@ -162,28 +159,23 @@ def server(input: Inputs, output: Outputs, session: Session):
         model_1_predictions = load_predictions_model_1()
         model_2_predictions = load_predictions_model_2()
         # Check if both files are provided
-        if (
-            observations is None
-            or model_1_predictions is None
-            or model_2_predictions is None
-        ):
+        if observations is None or model_1_predictions is None or model_2_predictions is None:
             return "Please upload all three files to see the result."
 
         # Calculate the relevant metrics and CIs
-        GMR_1, CI_GMR_1, GMR_2, CI_GMR_2, W_1, CI_W_1, W_2, CI_W_2, ratio, CI_ratio = (
-            calculate_metrics_comparison(
-                observations=observations,
-                model_1_predictions=model_1_predictions,
-                model_2_predictions=model_2_predictions,
-                output=True,
-            )
+        GMR_1, CI_GMR_1, GMR_2, CI_GMR_2, CRPS_results = calculate_metrics_comparison(
+            observations=observations,
+            model_1_predictions=model_1_predictions,
+            model_2_predictions=model_2_predictions,
         )
         return f"""
         Geometric-mean ratio model 1: {GMR_1} {[float(CI_GMR_1[0]), float(CI_GMR_1[1])]} \n
         Geometric-mean ratio model 2: {GMR_2} {[float(CI_GMR_2[0]), float(CI_GMR_2[1])]} \n
-        Wasserstein distance model 1: {W_1} {[float(CI_W_1[0]), float(CI_W_1[1])]} \n
-        Wasserstein distance model 2: {W_2} {[float(CI_W_2[0]), float(CI_W_2[1])]} \n
-        Ratio:                        {ratio} {[float(CI_ratio[0]), float(CI_ratio[1])]}"""
+        Average CRPS Model 1:         {CRPS_results['CRPS_1']} {[float(CRPS_results['CI(CRPS_1)'][0]), float(CRPS_results['CI(CRPS_1)'][1])]} \n
+        Average CRPS Model 2:         {CRPS_results['CRPS_2']} {[float(CRPS_results['CI(CRPS_2)'][0]), float(CRPS_results['CI(CRPS_2)'][1])]} \n
+        CRPS Skill score Model 1:     {CRPS_results['S(CRPS_1)']} {[float(CRPS_results['CI(S(CRPS_1))'][0]), float(CRPS_results['CI(S(CRPS_1))'][1])]} \n
+        CRPS Skill score Model 2:     {CRPS_results['S(CRPS_2)']} {[float(CRPS_results['CI(S(CRPS_2))'][0]), float(CRPS_results['CI(S(CRPS_2))'][1])]} \n
+        Ratio CRPS scores:            {CRPS_results['CRPS_ratio']} {[float(CRPS_results['CI(CRPS_ratio)'][0]), float(CRPS_results['CI(CRPS_ratio)'][1])]} \n"""
 
     @output
     @render.plot
@@ -192,11 +184,7 @@ def server(input: Inputs, output: Outputs, session: Session):
         model_1_predictions = load_predictions_model_1()
         model_2_predictions = load_predictions_model_2()
         # Check if both files are provided
-        if (
-            observations is None
-            or model_1_predictions is None
-            or model_2_predictions is None
-        ):
+        if observations is None or model_1_predictions is None or model_2_predictions is None:
             return
         plot = plot_comparison(
             observations=observations,
